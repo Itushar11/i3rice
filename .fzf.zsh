d8a71930a3b27e76fe7ab8fba52944d11c0f8bea
Tushar <53882037+Itushar11@users.noreply.github.com>
2019-11-07 19:26:04 +0000

    fzf conf for zsh

---
+++ .fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/itushar/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/itushar/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/itushar/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/itushar/.fzf/shell/key-bindings.zsh"
